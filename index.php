a3fff23ba0feb9230fb641e795c8cce8bbbcd463
Severin Fürbringer <severin@protonmail.ch>
2016-06-08 14:11:35 +0200

    Added table and title

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>M133 - AJAX demo</title>	
+	</head>
+	<body>
+		<h1>Persönliche Daten</h1>
+		<table border="0" cellpadding="3" cellspacing="3">
+			<tr>
+				<td><label for="vorname">Ihr Vorname:</label></td>
+				<td><input id="vorname" name="vorname"></td>
+			</tr>
+			<tr>
+				<td><label for="nachname">Ihr Nachname:</label></td>
+				<td><input id="nachname" name="nachname"></td>
+			</tr>
+			<tr>
+				<td><label for="ort">Ihr Wohnort:</label></td>
+				<td><input id="ort" name="ort"></td>
+			</tr>
+		</table>
+	
+	</body>
+</html>
