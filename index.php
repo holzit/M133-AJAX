89cc73d6afcb03b9f78f1bff4ad8078fd68c053d
Severin Fürbringer <severin@protonmail.ch>
2016-06-08 14:19:54 +0200

    Revert "Added license text."
    This reverts commit adc2bb416e9ce85f058d657a8491cfe42a64c92a.

--- index.php
+++ index.php
@@ -5,22 +5,20 @@
 	</head>
 	<body>
 		<h1>Persönliche Daten</h1>
-		<form>
-			<table border="0" cellpadding="3" cellspacing="3">
-				<tr>
-					<td><label for="vorname">Ihr Vorname:</label></td>
-					<td><input id="vorname" name="vorname"></td>
-				</tr>
-				<tr>
-					<td><label for="nachname">Ihr Nachname:</label></td>
-					<td><input id="nachname" name="nachname"></td>
-				</tr>
-				<tr>
-					<td><label for="ort">Ihr Wohnort:</label></td>
-					<td><input id="ort" name="ort"></td>
-				</tr>
-			</table>
-		</form>
+		<table border="0" cellpadding="3" cellspacing="3">
+			<tr>
+				<td><label for="vorname">Ihr Vorname:</label></td>
+				<td><input id="vorname" name="vorname"></td>
+			</tr>
+			<tr>
+				<td><label for="nachname">Ihr Nachname:</label></td>
+				<td><input id="nachname" name="nachname"></td>
+			</tr>
+			<tr>
+				<td><label for="ort">Ihr Wohnort:</label></td>
+				<td><input id="ort" name="ort"></td>
+			</tr>
+		</table>
 	
 	</body>
 </html>
