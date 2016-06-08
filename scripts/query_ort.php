8301bf897d95ca9331e40afda9556a1bd8081ce1
Severin Fürbringer <severin@protonmail.ch>
2016-06-08 14:58:15 +0200

    Added AJAX PHP Query script

---
+++ scripts/query_ort.php
@@ -0,0 +1,8 @@
+﻿<?php
+/* This file returns a recommendation */
+/*
+   TODO: Save input from GET
+   TODO: Database connection
+   TODO: Query fitting city name
+   TODO: Return city name by echo'ing
+*/
