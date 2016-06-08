1f996f96abef87b244a2f3536e311007d74a33ab
Severin Fürbringer <severin@protonmail.ch>
2016-06-08 14:41:40 +0200

    Added AJAX-Query skeleton script

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -1,4 +1,32 @@
 ﻿(function() {
 	/* DOM ready */
 
+	var ort_input = document.getElementById("ort");
+	var result_output = document.getElementById("result");
+
+	/*
+	 * Executes AJAX query every time 
+	 */
+	ort_input.onkeyup = function() {
+		var text = this.value; // The string the user has written so far 
+
+		if(text.length == 0) {
+			// Input field is empty
+			result_output.innerHTML = "";
+			// Done for now
+			return;
+		} else if(text.length >= 2) {
+			// User has written at least two chars
+			var ajax_request = new XMLHttpRequest();
+			ajax_request.onreadystatechange = function() {
+				// Request ready & received
+				if(ajax_request.readyState == 4 && ajax_request.status == 200) {
+					// Finally, output the recommendation!
+					result_output.innerHTML = ajax_request.responseText;
+				}
+			};
+			ajax_request.open("GET", "" /* TODO: .php query file */, true);
+			ajax_request.send();
+		}
+	};
 }) ();
