c2776b1a007d3b414d240074820d59c99c16ead6
MCHDStudio <info@mchd.ch>
2016-06-08 16:43:57 +0200

    Added Button and style

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
+
+<div class="content-c">
+
+
 		<h1>Persoenliche Daten</h1>
 	</br>
 		<form>
@@ -19,19 +23,26 @@
 
 				<div class="group">
 					<input id="nachname" name="nachname">
-					<span class="highlight"></span>
-					<span class="bar"></span>
+						<span class="highlight"></span>
+						<span class="bar"></span>
 					<label for="nachname">Ihr Nachname:</label>
 				</div>
 
 				<div class="group">
 					<input id="ort" name="ort">
-					<span class="highlight"></span>
-					<span class="bar"></span>
+						<span class="highlight"></span>
+						<span class="bar"></span>
 					<label for="ort">Ihr Wohnort:</label>
+					<span id="result"></span>
+				</div>
+
+				<div class="group">
+				<button class="buttonf" type="button" onclick="alert('Hello World!')">Send</button>
 				</div>
 
 		</form>
+
+</div>
 
 	</body>
 </html>
