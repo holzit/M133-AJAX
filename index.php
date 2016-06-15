5213c0ca51df4660c4ce2ebdbd7f1eb557daf931
Hoi15A <austin.aa@me.com>
2016-06-15 14:09:56 +0200

    Added Favicon aka web Emojis
    :rocket: :rocket: :rocket: :rocket:

--- index.php
+++ index.php
@@ -5,6 +5,28 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+		<!-- ICONS -->
+		<link rel="apple-touch-icon" sizes="57x57" href="img/ico/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/ico/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/ico/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/ico/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/ico/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/ico/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/ico/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/ico/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/ico/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="img/ico/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="img/ico/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="img/ico/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/ico/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="img/ico/manifest.json">
+		<link rel="shortcut icon" href="img/ico/favicon.ico">
+		<meta name="msapplication-TileColor" content="#16abc0">
+		<meta name="msapplication-TileImage" content="img/ico/mstile-144x144.png">
+		<meta name="msapplication-config" content="img/ico/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
+
 	</head>
 	<body>
 
